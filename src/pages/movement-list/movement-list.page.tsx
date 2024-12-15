3ae9b8e18bd881d553bcc3de1fdf53f58d666f4d
mariamoratalla <maria.moratalla97@gmail.com>
2024-12-15 23:27:18 +0100

    inicio app

--- src/pages/movement-list/movement-list.page.tsx
+++ src/pages/movement-list/movement-list.page.tsx
@@ -1,10 +1,57 @@
 import { AppLayout } from "@/layouts";
 import React from "react";
+import { MovementListVm } from "./movement-list.vm";
 
 export const MovementList: React.FC = () => {
+  const movimientos: MovementListVm[] = [
+    {
+      id: 1,
+      fecha: new Date(2019, 12, 9),
+      fechaValor: new Date(2019, 12, 9),
+      descripcion: "Nómina noviembre",
+      importe: 900,
+      saldoDisponible: 1000,
+    },
+    {
+      id: 2,
+      fecha: new Date(2019, 12, 7),
+      fechaValor: new Date(2019, 12, 3),
+      descripcion: "Alquiler noviembre",
+      importe: -400,
+      saldoDisponible: 500,
+    },
+    {
+      id: 3,
+      fecha: new Date(2019, 12, 6),
+      fechaValor: new Date(2019, 12, 9),
+      descripcion: "Mercadona",
+      importe: -100,
+      saldoDisponible: 400,
+    },
+    {
+      id: 5,
+      fecha: new Date(2019, 12, 23),
+      fechaValor: new Date(2019, 12, 9),
+      descripcion: "Extra diciembre",
+      importe: 900,
+      saldoDisponible: 1300,
+    },
+  ];
+
   return (
     <AppLayout>
       <div>Movement list</div>
+      {movimientos.map((movimiento) => {
+        return (
+          <div>
+            <span key={movimiento.id}>{movimiento.fecha.toString()}</span>
+            <span key={movimiento.id}>{movimiento.fechaValor.toString()}</span>
+            <span key={movimiento.id}>{movimiento.descripcion}</span>
+            <span key={movimiento.id}>{movimiento.importe}</span>
+            <span key={movimiento.id}>{movimiento.saldoDisponible}</span>
+          </div>
+        );
+      })}
     </AppLayout>
   );
 };
---
+++ src/pages/movement-list/movement-list.vm.ts
@@ -0,0 +1,8 @@
+export interface MovementListVm {
+  id: number;
+  fecha: Date;
+  fechaValor: Date;
+  descripcion: string;
+  importe: number;
+  saldoDisponible: number;
+}
